dd7f22a4b8acd803e4080b64ddc43b9e529026f7
dlm <denglm1227@qq.com>
2025-08-15 11:47:41 +0800

    推送数据first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using WpfAppAi.Model;
+
+
+namespace WpfAppAi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            // 获取应用启动的进程名
+            string processName = Process.GetCurrentProcess().ProcessName;
+            int count = Process.GetProcessesByName(processName).Length;
+
+            if (count > 1)
+            {
+                Environment.Exit(0); // 强制退出
+            }
+
+            //MessageBox.Show("请插入加密狗");
+            // 直接退出应用程序
+            //Environment.Exit(0); // 强制退出
+            base.OnInitialized(e);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            ProcessManager.CloseAllJobObjects();
+        }
+
+        // 一键唤醒所有服务
+        private async Task FullAutoStartServer() {
+            try
+            {
+                StopServer.IsEnabled = true;
+                StartServer.IsEnabled = false;
+                foreach (Item item in StartProgressUtil.Instance.ItemList)
+                {
+                    string args = " ";
+                    foreach (string arg in item.Args)
+                    {
+                        args = args + "" + arg; ;
+                    }
+
+                    if (item.Port != 0)
+                    {
+                        bool isUse = ProcessManager.IsPortInUse(item.Port);
+                        if (isUse)
+                        {
+                            throw new Exception(item.Port + "端口被占用，请手动释放此端口，或更换端口\n");
+                        }
+                    }
+
+                    ProcessManager.StartProcessWithJobObject(item.Key,item.FilePath,args,ServerLog);
+                    ServerLog.AppendText("服务启动中...：\n");
+                    if (item.Port != 0)
+                    {
+                        bool isOccupied = await ProcessManager.WaitForPortOccupiedAsync(item.Port);
+                        if (!isOccupied)
+                        {
+                            throw new Exception(item.Key + "未能成功启动，请查询对应服务配置信息\n");
+                        }
+                    }
+
+                    if (item.WaitForExit)
+                    {
+                        ServerLog.AppendText(item.Key+"等待进程任务完成...：\n");
+                        await ProcessManager.WaitForJobExitAsync(item.Key);
+                        ServerLog.AppendText(item.Key + "进程已结束：\n");
+                    }
+                }
+                ServerLog.AppendText("服务启动成功：\n");
+            }
+            catch (Exception e){
+                ServerLog.AppendText("启动过程发生异常：\n");
+                ServerLog.AppendText(e.Message);
+                ProcessManager.CloseAllJobObjects();
+                ServerLog.AppendText("已终止并关闭所有已启动进程");
+                StopServer.IsEnabled = false;
+                StartServer.IsEnabled = true;
+            }
+        }
+
+        private void ServerLog_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                textBox.ScrollToEnd();
+            }
+        }
+
+        private async void StartServer_Click(object sender, RoutedEventArgs e)
+        {
+            StopServer.IsEnabled = true;
+            StartServer.IsEnabled = false;
+            await FullAutoStartServer();
+        }
+
+        private void StopServer_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessManager.CloseAllJobObjects();
+            ServerLog.AppendText("所有服务已停止\n");
+            StopServer.IsEnabled = false;
+            StartServer.IsEnabled = true;
+        }
+
+        
+    }
+
+}
---
+++ Model/Items.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WpfAppAi.Model
+{
+    [XmlRoot("Items")]
+    public class Items
+    {
+        [XmlElement("Item")]
+        public List<Item> ItemList { get; set; } = new List<Item>();
+
+    }
+
+    public class Item {
+
+        [XmlElement("Key")]
+        public string Key { get; set; } = string.Empty;
+
+        [XmlElement("FilePath")]
+        public string FilePath { get; set; } = string.Empty;
+
+        [XmlElement("Port")]
+        public int Port { get; set; }
+
+        [XmlElement("WaitForExit")]
+        public bool WaitForExit { get; set; }
+
+        [XmlArray("Args")]
+        [XmlArrayItem("Arg")]
+        public List<string> Args { get; set; } = new List<string>();
+    }
+
+}
